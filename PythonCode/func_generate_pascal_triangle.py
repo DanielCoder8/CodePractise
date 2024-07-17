5ead331c245c2c6ef2ada44d7ad47bc3cfe3b2a3
DanielCoder8 <daniel_thompson78@163.com>
2024-07-17 19:07:56 +0800

    Create func_generate_pascal_triangle.py

---
+++ PythonCode/func_generate_pascal_triangle.py
@@ -0,0 +1,12 @@
+# func_generate_pascal_triangle.py
+def func_generate_pascal_triangle(n):
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
+
+print(func_generate_pascal_triangle(5))
