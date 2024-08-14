59e684bba5e9701b6a342030b2bab58846c4a8f5
DanielCoder8 <daniel_thompson78@163.com>
2024-08-14 20:10:03 +0800

    Create func_py_calculate_hyperboloid_volume.py

---
+++ PythonCode/func_py_calculate_hyperboloid_volume.py
@@ -0,0 +1,7 @@
+# func_py_calculate_hyperboloid_volume.py
+import math
+
+def func_py_calculate_hyperboloid_volume(radius, height):
+    return math.pi * radius**2 * height
+
+print(func_py_calculate_hyperboloid_volume(3, 8))
