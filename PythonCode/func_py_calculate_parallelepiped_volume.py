befd33b049184b855aedf62a54db723808d51f35
DanielCoder8 <daniel_thompson78@163.com>
2024-08-16 20:39:02 +0800

    Create func_py_calculate_parallelepiped_volume.py

---
+++ PythonCode/func_py_calculate_parallelepiped_volume.py
@@ -0,0 +1,5 @@
+# func_py_calculate_parallelepiped_volume.py
+def func_py_calculate_parallelepiped_volume(length, width, height):
+    return length * width * height
+
+print(func_py_calculate_parallelepiped_volume(5, 3, 8))
